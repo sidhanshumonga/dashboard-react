8ddd5670aa960a8d66db185040388a74527ab7eb
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2020-07-06 03:53:37 +0530

    added docs

---
+++ docs/precache-manifest.e64175232e268d5c3a676d26d5aff0ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c07445157d7f9dc032f1fd5317958d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a19a9126e1340e272c9",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f83ceb248566243c2ab7",
+    "url": "/static/css/main.caf66eee.chunk.css"
+  },
+  {
+    "revision": "1a19a9126e1340e272c9",
+    "url": "/static/js/2.2891e580.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/static/js/2.2891e580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83ceb248566243c2ab7",
+    "url": "/static/js/main.f542e00e.chunk.js"
+  },
+  {
+    "revision": "4e6e0075ae20fae7b0c7",
+    "url": "/static/js/runtime-main.0e4ce446.js"
+  }
+]);
